8e207e35088f41bb99dea435c203a3fa931affef
Alejandro Yela <andres.alejo.yela@gmail.com>
2024-04-22 17:44:25 -0500

    Add json first GET

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const http = require('node:http')
+const PORT = 4321
 
-const PORT = process.env.PORT ?? 4321
+const productosJSON = require('./products/products.json')
 
-const server = http.createServer((req, res) => {
+const processRequest = (req, res) => {
+    const { method, url } = req
+    switch (method) {
+        case 'GET':
+            switch (url) {
+                case '/productos':
+                    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+                    return res.end(JSON.stringify(productosJSON))
+                default:
+                    res.statusCode = 404
+                    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+                    return res.end('<h1>404</h1>')
+            }
+    }
 
-    console.log(`request received: ${req.url}`);
-    res.end('Hola mundo')
-})
+}
 
-server.listen(PORT, ()=>{
-    console.log(`Server listening at port http://localhost:${PORT}`);
+const server = http.createServer(processRequest)
+
+server.listen(1234, () => {
+    console.log(`server linstening on port http://localhost:${PORT}`);
 })
