139f419d34567d43fa67e8df129de7fc4ff11181
Alejandro Yela <andres.alejo.yela@gmail.com>
2024-04-23 01:00:44 -0500

    Functional GET and id filter endpoints, and new json database

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
-const http = require('node:http')
-const PORT = 4321
+const express = require('express')
+const cors = require('cors');
+const app = express()
 
-const productosJSON = require('./products/products.json')
+app.use(cors());
 
-const processRequest = (req, res) => {
-    const { method, url } = req
-    switch (method) {
-        case 'GET':
-            switch (url) {
-                case '/productos':
-                    res.setHeader('Content-Type', 'application/json; charset=utf-8')
-                    return res.end(JSON.stringify(productosJSON))
-                default:
-                    res.statusCode = 404
-                    res.setHeader('Content-Type', 'text/html; charset=utf-8')
-                    return res.end('<h1>404</h1>')
-            }
+app.disable('x-powered-by')
+
+const productosJSON = require('./products/products.json');
+const PORT = process.env.PORT ?? 4321
+
+app.use(express.json())
+
+app.get('/productos', (req, res) => {
+
+    const { categoria, tag_list } = req.query
+
+    if (categoria) {
+        const filteredProducts = productosJSON.filter(
+            producto => producto.categoria && producto.categoria.toLowerCase() === categoria.toLowerCase()
+        )
+        return res.json(filteredProducts)
     }
 
-}
+    if (tag_list) {
 
-const server = http.createServer(processRequest)
+        const filteredProducts = productosJSON.filter(
+            producto => producto.tag_list.some(t => t.toLowerCase() === tag_list.toLowerCase())
+        )
+        return res.json(filteredProducts)
+    }
+    res.json(productosJSON)
+})
 
-server.listen(1234, () => {
-    console.log(`server linstening on port http://localhost:${PORT}`);
+
+app.get('/productos/:id', (req, res) => {
+    const { id } = req.params
+    const producto = productosJSON.find(producto => producto.id == id)
+    if (productosJSON) return res.json(producto)
+
+    res.status(404).json({ message: 'Product not found' })
+})
+
+app.post('/productos', (req, res) => {
+    const { brand, name, price } = req.body
+    res.status(201).json(req.body)
+})
+
+app.use((req, res) => {
+    res.status(404).redirect('http://http.cat/404');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}`);
 })
